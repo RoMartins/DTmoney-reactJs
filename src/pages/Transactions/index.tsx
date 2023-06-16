71297e23f00e7e0f93a487a00a5f30ffdf02a816
Rodrigo <rodrigomartins_santos1804@hotmail.com>
2023-06-16 15:38:28 -0300

    feat:create tableTransaction on component trasanctions

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,33 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { ColumnPrice, TransactionsContainer, TransactionsTable } from "./styles";
 
 export function Transactions() {
   return (
     <div>
       <Header />
       <Summary />
+
+       <TransactionsContainer>
+        
+      <TransactionsTable >
+        <tbody>
+          <tr>
+            <td width='50%'>Desenvolvimento do site</td>
+            <ColumnPrice typeTransaction="income">R$ 17.400,00</ColumnPrice>
+            <td>Venda</td>
+            <td>13/04/2022</td>
+          </tr>
+
+          <tr>
+            <td width='50%'>Hamburguer</td>
+            <ColumnPrice typeTransaction="outcome">R$ 400,00</ColumnPrice>
+            <td>Alimentação</td>
+            <td>15/04/2022</td>
+          </tr>
+        </tbody>
+      </TransactionsTable>
+       </TransactionsContainer>
     </div>
   )
 }
